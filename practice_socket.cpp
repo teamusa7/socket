9ce9e1e41c5b9382de4502331d5c7c836e4da521
Andres Marin <chito7000@yahoo.com>
2023-03-26 14:13:12 -0700

    added server file

--- practice_socket.cpp
+++ practice_socket.cpp
@@ -1,8 +1,33 @@
-//
-//  practice_socket.cpp
-//  
-//
-//  Created by Andres Marin on 3/19/23.
-//
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
 
-#include <stdio.h>
+
+void error (char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    
+    //file descriptors
+    int sockfd, newsockfd, portno, client_len, n;
+    
+    char bufer[256];
+    
+    struct sockaddr_in serv_addr, cli_addr;
+    
+    if(argc < 2) {
+        error("ERROR, no port provided");
+    }
+    
+    //create a socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error
+    }
+    
+    
+    
+}
